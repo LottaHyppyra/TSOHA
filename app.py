120abbada5af22b37bf23e36ec67da01480e4fbf
Lotta Hyppyrä <lotta@hyppyra.net>
2022-09-02 17:30:57 +0300

    Muokkaa app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask import render_template
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def etusivu():
-    return render_template("layout.html")
-
+import routes
